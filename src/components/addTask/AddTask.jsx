424330ec037c597cfafb521de04e9cdcc2acb277
humEyra1064 <humeyraercanfurkanyilmaz@gmail.com>
2023-02-23 12:40:16 +0300

    header added

--- src/components/addTask/AddTask.jsx
+++ src/components/addTask/AddTask.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const AddTask = () => {
   return (
     <div>
-      
-    </div>
-  )
-}
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Task</Form.Label>
+          <Form.Control type="text" />
+        </Form.Group>
 
-export default AddTask
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Date</Form.Label>
+          <Form.Control type="date" />
+        </Form.Group>
+
+        <div className="text-center">
+          <Button variant="primary w-50" type="submit">
+            Save
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
+};
+
+export default AddTask;
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import AddTask from '../components/addTask/AddTask'
 import TaskList from '../components/taskList/TaskList'
 
