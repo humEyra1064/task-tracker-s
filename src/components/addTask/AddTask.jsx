b9db8354709d931c52b569f97c3d3513463b0be0
humEyra1064 <humeyraercanfurkanyilmaz@gmail.com>
2023-02-23 16:02:54 +0300

    tasklist create

--- src/components/addTask/AddTask.jsx
+++ src/components/addTask/AddTask.jsx
@@ -1,5 +1,4 @@
 import axios from "axios";
-import React from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { useState } from "react";
--- src/components/taskList/TaskList.jsx
+++ src/components/taskList/TaskList.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import axios from 'axios';
+
+import {RiDeleteBack2Fill  } from 'react-icons/ri'
 
 const TaskList = ({task,getTask}) => {
+  const deleteTask =async(id)=>{
+const url ="https://63f7293fe40e087c9588886d.mockapi.io/api/tasks";
+try {
+  await axios.delete(`${url}/${id}`)
+} catch (error) {
+  console.log(error)
+  
+}
+getTask()
+  }
   return (
     <div>
-      
+      {task.map((item)=>{
+       const {id,task,date} = item 
+       return(
+      <div>
+        <div>
+          <h4>{task}</h4>
+        <p>{date}</p> 
+        </div>
+        <div>
+        <RiDeleteBack2Fill
+        style={{cursor:"pointer",
+      marginRight:"20px",
+    fontSize:"2rem",
+  boxShadow:"2px 2px 2px green"}}
+        onClick={()=>deleteTask(id)}
+        />
+        </div>
+       
+       </div>
+       
+       )
+      })}
     </div>
   )
 }
