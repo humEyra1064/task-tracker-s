99e58e54b3f4f47e89242dc25096677b1389c060
humEyra1064 <humeyraercanfurkanyilmaz@gmail.com>
2023-02-23 13:02:19 +0300

    addtask added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './page/Home';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container mt-5">
      <Header/>
      <Home/>
     </div>
--- src/components/addTask/AddTask.jsx
+++ src/components/addTask/AddTask.jsx
@@ -1,19 +1,37 @@
+import axios from "axios";
 import React from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { useState } from "react";
 
 const AddTask = () => {
+    const [task, setTask] = useState("")
+    const [date, setDate] = useState("")
+
+    const handleSubmit =(e)=>{
+        e.preventDefault();
+        const newTask ={task,date}
+        addNewTask(newTask)
+    }
+    const addNewTask =async(newTask)=>{
+        const url ="https://63f7293fe40e087c9588886d.mockapi.io/api/tasks";
+        try {
+           await axios.post(url,newTask) 
+        } catch (error) {
+            
+        }
+    }
   return (
     <div>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Task</Form.Label>
-          <Form.Control type="text" />
+          <Form.Control type="text" placeholder="Enter Task" onChange={(e)=>setTask(e.target.value)}/>
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Date</Form.Label>
-          <Form.Control type="date" />
+          <Form.Control type="date"  onChange={(e)=>setDate(e.target.value)} />
         </Form.Group>
 
         <div className="text-center">
